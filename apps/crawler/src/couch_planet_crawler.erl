AlexMikhalev/couch_planet

commit 57f1716f0f8d5bac34406ae93c388fd663d2fad5

more meaningful logging

apps/crawler/src/couch_planet_crawler.erl
@@ -137,7 +137,7 @@ parse_bulk_docs_response(B) ->
                 {error, "no 'id' field in bulk update response"};
             {<<"id">>, Id} ->
                 case lists:keyfind(<<"rev">>, 1, TupleList) of
-                false -> {error, "no 'rev' field in bulk update response"};
+                false -> {error, "could not update '" ++ ?b2l(Id)} ++ "'";
                 {<<"rev">>, Rev} -> [{Id, Rev}|Acc]
                 end
             end

commit 1f64ab76a3a12a5ea8977526b2f8a6821d7f0574

Improve error logging and fix an issue introduced with 57f1716f.

apps/crawler/src/couch_planet_crawler.erl
@@ -137,8 +137,15 @@ parse_bulk_docs_response(B) ->
                 {error, "no 'id' field in bulk update response"};
             {<<"id">>, Id} ->
                 case lists:keyfind(<<"rev">>, 1, TupleList) of
-                false -> {error, "could not update '" ++ ?b2l(Id)} ++ "'";
-                {<<"rev">>, Rev} -> [{Id, Rev}|Acc]
+                false ->
+                    case lists:keyfind(<<"error">>, 1, TupleList) of
+                    false ->
+                        {error, "could not update"};
+                    {<<"error">>, Error} ->
+                        {error, "could not update: " ++ ?b2l(Error)}
+                    end;
+                {<<"rev">>, Rev} ->
+                    [{Id, Rev}|Acc]
                 end
             end
         end, [], L).