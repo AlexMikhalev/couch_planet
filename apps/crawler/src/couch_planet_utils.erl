79d07ca69ab9a0192acf64eae8eaf0facdb0eda6
Klaus Trainer <klaus.trainer@web.de>
2010-12-15 17:34:16 +0100

    Fix issue with strings like "\u9999" in feed content.

--- apps/crawler/src/couch_planet_utils.erl
+++ apps/crawler/src/couch_planet_utils.erl
@@ -30,11 +30,20 @@ xml2xml_text2json_text(Xml) ->
     XmlText1 = re:replace(XmlText0, <<"&lt;">>, <<"<">>, [global, {return, binary}]),
     XmlText2 = re:replace(XmlText1, <<"&amp;">>, <<"\\&">>, [global, {return, binary}]),
     JsonText0 = re:replace(XmlText2, <<"\"">>, <<"\\\\\"">>, [global, {return, binary}]),
-    case get(json_regex) of
+    case get(json_regex_1) of
     undefined ->
         {ok, MP1} = re:compile(<<"(\\\\\\\\)+\"">>),
-        put(json_regex, MP1);
+        put(json_regex_1, MP1);
     MP1 ->
         ok
     end,
-    re:replace(JsonText0, MP1, <<"\\1\\\\\"">>, [global, {return, binary}]).
+    JsonText1 = re:replace(JsonText0, MP1, <<"\\1\\\\\"">>, [global, {return, binary}]),
+    JsonText2 = re:replace(JsonText1, <<"\\\\u">>, <<"\\\\\\\\u">>, [global, {return, binary}]),
+    case get(json_regex_2) of
+    undefined ->
+        {ok, MP2} = re:compile(<<"(\\\\\\\\)+\\\\u">>),
+        put(json_regex_2, MP2);
+    MP2 ->
+        ok
+    end,
+    re:replace(JsonText2, MP2, <<"\\1\\\\\\\\u">>, [global, {return, binary}]).
